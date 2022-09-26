8f0ff4dc8a5d78644507b77874cb3d045ce7b5f9
ram <ramzzprakash.j.d2029@gmail.com>
2022-09-26 23:55:40 +0530

    5th

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
     //  name
     var player1 = "Player 1";
     var sum=0; 
+    var num=0;
+    var current=0;
     function editNames() {
         player1 = prompt("Change Player1 name");
         document.querySelector("p.Player1").innerHTML = player1;
@@ -11,12 +13,23 @@
              document.querySelector(".img1").setAttribute("src",
                 "assets/face0.gif");
         setTimeout(function () {
-            var current = Math.floor(Math.random() * 6) + 1;
+         current = Math.floor(Math.random() * 6) + 1;
   
             document.querySelector(".img1").setAttribute("src",
-                "assets/face" + current + ".png");
-		sum+=current;
-                console.log(sum);
+                "assets/face" + current + ".png"); 
+if(current==1){
+	num++;
+}
+playGame();
+
           
-        } ,2500);
+        } ,1500);
     }
+function playGame(){
+   if(num>0){
+      sum+=current;
+      console.log(sum);	
+$("#"+sum+"").css("color","red");
+
+}
+}
