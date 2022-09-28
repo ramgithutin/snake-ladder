5c09d74388d4bc3c8f6319ff0663b0002567bb61
ram <ramzzprakash.j.d2029@gmail.com>
2022-09-28 18:13:40 +0530

    11

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
-    //  name
-    var player1 = "Player 1";
+
     var sum = 0;
     var sum2 = 0;
-    var num = 1;
+    var num = 0;
+    var num2=0;
     var current = 0;
     var p = 0;
     var n = 0;
-
-    function editNames() {
-    	player1 = prompt("Change Player1 name");
-    	document.querySelector("p.Player1").innerHTML = player1;
-    }
-
-    // Function to roll the dice
     function rollTheDice() {
     	document.querySelector(".img1").setAttribute("src", "assets/face0.gif");
     	//$("#"+sum+"").remove(":contains('A')");
@@ -23,146 +16,151 @@
 
     		document.querySelector(".img1").setAttribute("src",
     			"assets/face" + current + ".png");
-    		if (current == 1) {
-    			num++;
-    		}
-    		playGame();
+                if (n % 2 == 0) {
+			player2();
+			document.querySelector('#Play').innerHTML = "Player1's turn"
 
+    			if (current == 1) {
+    				num2=1;
+    				
+			}
+    		} else if(n%2!=0) {
+			document.querySelector('#Play').innerHTML = "Player2's turn"
+			player();
+    			if (current == 1) {
+    				num=1;
+    			}
 
+		}
     	}, 100);
-    }
-
-    function playGame() {
-    	if (num > 0) {
-    		if (n % 2 == 0) {
-    			player2();
-    		} else {
-    			player1();
+    }    	
+    	
+    	function player() {
+		if (num > 0) {
+    			$("#img2").remove();
+    			sum += current;
+    			if (sum == 27) {
+    				sum = 5;
+    				play();
+    			} else if (sum == 40) {
+    				sum = 3;
+    				play();
+    			} else if (sum == 43) {
+    				sum = 18;
+    				play();
+    			} else if (sum == 54) {
+    				sum = 31;
+    				play();
+    			} else if (sum == 66) {
+    				sum = 45;
+    				play();
+    			} else if (sum == 76) {
+    				sum = 58;
+    				play();
+    			} else if (sum == 89) {
+    				sum = 53;
+    				play();
+    			} else if (sum == 99) {
+    				sum = 41;
+    				play();
+    			}
+    			//ladder
+    			else if (sum == 4) {
+    				sum = 25;
+    				play();
+    			} else if (sum == 13) {
+    				sum = 46;
+    				play();
+    			} else if (sum == 33) {
+    				sum = 49;
+    				play();
+    			} else if (sum == 42) {
+    				sum = 63;
+    				play();
+    			} else if (sum == 50) {
+    				sum = 69;
+    				play();
+    			} else if (sum == 62) {
+    				sum = 81;
+    				play();
+    			} else if (sum == 74) {
+    				sum = 92;
+    				play();
+    			} else if (sum > 100) {
+    				sum -= current;
+    				play();
+    			} else {
+    				play();
+    			}
+    			if (sum == 100) {
+				$(".container2").remove();
+    				alert("player1 win")
+    			}
     		}
-    	}
-
-    	function player1() {
-    		$("#img2").remove();
-    		sum += current;
-    		if (sum == 27) {
-    			sum = 5;
-    			play();
-    		} else if (sum == 40) {
-    			sum = 3;
-    			play();
-    		} else if (sum == 43) {
-    			sum = 18;
-    			play();
-    		} else if (sum == 54) {
-    			sum = 31;
-    			play();
-    		} else if (sum == 66) {
-    			sum = 45;
-    			play();
-    		} else if (sum == 76) {
-    			sum = 58;
-    			play();
-    		} else if (sum == 89) {
-    			sum = 53;
-    			play();
-    		} else if (sum == 99) {
-    			sum = 41;
-    			play();
-    		}
-    		//ladder
-    		else if (sum == 4) {
-    			sum = 25;
-    			play();
-    		} else if (sum == 13) {
-    			sum = 46;
-    			play();
-    		} else if (sum == 33) {
-    			sum = 49;
-    			play();
-    		} else if (sum == 42) {
-    			sum = 63;
-    			play();
-    		} else if (sum == 50) {
-    			sum = 69;
-    			play();
-    		} else if (sum == 62) {
-    			sum = 81;
-    			play();
-    		} else if (sum == 74) {
-    			sum = 92;
-    			play();
-    		} else if (sum > 100) {
-    			sum -= current;
-    			play();
-    		} else {
-    			play();
-    		}
-    		if (sum == 100) {
-    			alert("player1 win")
-    		}
-    	}
-
+	}
     	function player2() {
-    		$("#img3").remove();
-    		sum2 += current;
-    		if (sum2 == 27) {
-    			sum2 = 5;
-    			play2();
-    		} else if (sum2 == 40) {
-    			sum2 = 3;
-    			play2();
-    		} else if (sum2 == 43) {
-    			sum2 = 18;
-    			play2();
-    		} else if (sum2 == 54) {
-    			sum2 = 31;
-    			play2();
-    		} else if (sum2 == 66) {
-    			sum2 = 45;
-    			play2();
-    		} else if (sum2 == 76) {
-    			sum2 = 58;
-    			play2();
-    		} else if (sum2 == 89) {
-    			sum2 = 53;
-    			play2();
-    		} else if (sum2 == 99) {
-    			sum2 = 41;
-    			play2();
+		if (num2 > 0) {
+    			$("#img3").remove();
+    			sum2 += current;
+    			if (sum2 == 27) {
+    				sum2 = 5;
+    				play2();
+    			} else if (sum2 == 40) {
+    				sum2 = 3;
+    				play2();
+    			} else if (sum2 == 43) {
+    				sum2 = 18;
+    				play2();
+    			} else if (sum2 == 54) {
+    				sum2 = 31;
+    				play2();
+    			} else if (sum2 == 66) {
+    				sum2 = 45;
+    				play2();
+    			} else if (sum2 == 76) {
+    				sum2 = 58;
+    				play2();
+    			} else if (sum2 == 89) {
+    				sum2 = 53;
+    				play2();
+    			} else if (sum2 == 99) {
+    				sum2 = 41;
+    				play2();
+    			}
+    			//ladder
+    			else if (sum2 == 4) {
+    				sum2 = 25;
+    				play2();
+    			} else if (sum2 == 13) {
+    				sum2 = 46;
+    				play2();
+    			} else if (sum2 == 33) {
+    				sum2 = 49;
+    				play2();
+    			} else if (sum2 == 42) {
+    				sum2 = 63;
+    				play2();
+    			} else if (sum2 == 50) {
+    				sum2 = 69;
+    				play2();
+    			} else if (sum2 == 62) {
+    				sum2 = 81;
+    				play2();
+    			} else if (sum2 == 74) {
+    				sum2 = 92;
+    				play2();
+    			} else if (sum2 > 100) {
+    				sum2 -= current;
+    				play2();
+    			} else {
+    				play2();
+    			}
+    			if (sum2 == 100) {
+				$(".container2").remove();
+    				alert("player2 win")
+    			}
     		}
-    		//ladder
-    		else if (sum2 == 4) {
-    			sum2 = 25;
-    			play2();
-    		} else if (sum2 == 13) {
-    			sum2 = 46;
-    			play2();
-    		} else if (sum2 == 33) {
-    			sum2 = 49;
-    			play2();
-    		} else if (sum2 == 42) {
-    			sum2 = 63;
-    			play2();
-    		} else if (sum2 == 50) {
-    			sum2 = 69;
-    			play2();
-    		} else if (sum2 == 62) {
-    			sum2 = 81;
-    			play2();
-    		} else if (sum2 == 74) {
-    			sum2 = 92;
-    			play2();
-    		} else if (sum2 > 100) {
-    			sum2 -= current;
-    			play2();
-    		} else {
-    			play2();
-    		}
-    		if (sum2 == 100) {
-    			alert("player2 win")
-    		}
-    	}
-
+	}
     	function play() {
 
     		$('#' + sum + "").append('<img id="img2" src="assets/td.gif" />');
@@ -174,8 +172,5 @@
     	function play2() {
 
     		$('#' + sum2 + "").append('<img id="img3" src="assets/md.gif" />');
-    		console.log("p1" + sum2);
-
-
-    	}
-    }
+    		console.log("2nd" + sum2);
+	}
